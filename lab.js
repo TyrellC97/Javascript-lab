0b7c04ea54eb72a475aeea0d03fb255baab3239c
tyrellc97 <tyrellcoleman97@gmail.com>
2024-02-11 15:00:49 -0500

    question2

--- lab.js
+++ lab.js
@@ -47,3 +47,52 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
 
 let isDivis5 = (( n1 % 5 && n2 % 5 && n3 % 5 && n4 % 5) == 0)
 
+let isNum1Big = (n1 > n4)
+console.log(isNum1Big)
+
+console.log((n2 - n1) * n3 % n4)
+
+
+// Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
+
+const isThisOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25; 
+
+
+// Part 2: Practical Math
+// Let’s look at a more practical scenario.
+// You are planning a cross-country road trip!
+// The distance of the trip, in total, is 1,500 miles. 
+
+
+
+// Your car’s fuel efficiency is as follows:
+// At 55 miles per hour, you get 30 miles per gallon.
+// At 60 miles per hour, you get 28 miles per gallon.
+// At 75 miles per hour, you get 23 miles per gallon.
+// You have a fuel budget of $175.
+// The average cost of fuel is $3 per gallon.
+// Set up a program to answer the following questions:
+// How many gallons of fuel will you need for the entire trip?
+// Will your budget be enough to cover the fuel expense?
+// How long will the trip take, in hours?
+
+const tripDist = 1500
+const fuelCost = 3
+const budget = 175 
+let mpg = 30
+let mph = 55
+
+
+let howMuchFuel = tripDist / mpg 
+console.log(howMuchFuel + " gallons needed")
+ 
+let enoughMoney = howMuchFuel * fuelCost
+console.log(enoughMoney + " dollars")
+
+let hours = tripDist / mph
+console.log(hours + " hours away")
+ 
+
+
+ 
+
